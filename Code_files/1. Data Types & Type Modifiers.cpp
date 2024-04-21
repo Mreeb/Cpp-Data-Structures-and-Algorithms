a579ef325b718d6d41d5c18be0a1300e34d88ab9
Muhammad Areeb Khan <103059817+Mreeb@users.noreply.github.com>
2024-04-21 13:20:00 +0500

    Add files via upload

---
+++ Code_files/1. Data Types & Type Modifiers.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+    int a;
+    cout<<"Size of Int: "<<sizeof(a)<<"\n";
+    
+    float b;
+    cout<<"Size of Float: "<<sizeof(b)<<"\n";
+    
+    char c;
+    cout<<"Size of Character: "<<sizeof(c)<<"\n";
+    
+    bool d;
+    cout<<"Size of Boolean: "<<sizeof(d)<<"\n";
+    
+    
+    
+    short int si;
+    cout<<"Size of short Int: "<<sizeof(si)<<"\n";
+    
+    long int li;
+    cout<<"Size of long Int: "<<sizeof(li)<<"\n";
+    
+    return 0;
+    
+}
